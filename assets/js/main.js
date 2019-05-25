0cee72185cc5c1cf09ab86ae6608be285b44ece0
Dalia Tariq <daliatariq95@gmail.com>
2019-05-26 00:41:20 +0300

    client

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,7 +57,24 @@ $('#WebDevelopment').LineProgressbar({
  * OWL
  *****/
 
-    $(".owl-carousel").owlCarousel();
+    $(".owl-carousel").owlCarousel(
+        {
+            loop:true,
+            margin:10,
+            nav:false,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+                1000:{
+                    items:1
+                }
+            }
+        }
+    );
  
 
 
