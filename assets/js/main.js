7ac48f356e0649d31f2bf68141d49352d68903e7
Dalia Tariq <daliatariq95@gmail.com>
2019-06-09 18:27:43 +0300

    Change navigation active state on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,45 @@ $(document).ready(function () {
 
     //
     //Default active on home
+    $(document).ready(function () {
+        $(document).on("scroll", onScroll);
+        
+        //smoothscroll
+        $('.nav-link[href^="#"]').on('click', function (e) {
+            e.preventDefault();
+            $(document).off("scroll");
+            
+            $('a').each(function () {
+                $(this).removeClass('active');
+            })
+            $(this).addClass('active');
+          
+            var target = this.hash,
+                menu = target;
+            $target = $(target);
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top+2
+            }, 500, 'swing', function () {
+                window.location.hash = target;
+                $(document).on("scroll", onScroll);
+            });
+        });
+    });
     
+    function onScroll(event){
+        var scrollPos = $(document).scrollTop();
+        $('.navbar-nav li a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $('#menu-center ul li a').removeClass("active");
+                currLink.addClass("active");
+            }
+            else{
+                currLink.removeClass("active");
+            }
+        });
+    }
     /******************Gellary*******************/
        //ctive Navbar Links
        $('.nav-item .nav-link').click(function(){
